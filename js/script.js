eb9f9c442758541d474abe56ecc1afbcc41c55c6
Pablo Portilla Rivero <pabloportilla7@gmail.com>
2024-02-27 17:47:02 +0100

    Se añade la funcionalidad de mostrar en el html las peliculas de thriller y alguna modificacion en el archivo style

--- js/script.js
+++ js/script.js
@@ -12,15 +12,41 @@ console.log(arr_aventura.length);
 let div_gen_accion=document.getElementById('genero-28');
 
 for(let i=0;i<arr_accion.length;i++){
+    //Creo un div para cada pelicula y las coloco como hijo de la div genero-28
     let div_gen_accion_peliculas=document.createElement('div');
     div_gen_accion_peliculas.id='Pelicula_accion'+i;
     div_gen_accion.appendChild(div_gen_accion_peliculas);
+
+    //Creo un titulo y una imagen y los introduzco como hijo de cada div de cada pelicula
     let titulo=document.createElement('h3');
     let image=document.createElement('img');
     div_gen_accion_peliculas.appendChild(image);
     div_gen_accion_peliculas.appendChild(titulo);
+
+    //Les doy valor al titulo y a la imagen
     image.src='https://image.tmdb.org/t/p/w500'+arr_accion[i].poster_path;
     titulo.textContent=arr_accion[i].title;
+}
+
+
+//Para las peliculas de thriller
+let div_gen_thriller=document.getElementById('genero-53');
+
+for(let j=0;j<arr_thriller.length;j++){
+    //Creo un div para cada pelicula y las coloco como hijo de la div genero-28
+    let div_gen_thriller_peliculas=document.createElement('div');
+    div_gen_thriller_peliculas.id='Pelicula_thriller'+j;
+    div_gen_thriller.appendChild(div_gen_thriller_peliculas);
+
+    //Creo un titulo y una imagen y los introduzco como hijo de cada div de cada pelicula
+    let titulo=document.createElement('h3');
+    let image=document.createElement('img');
+    div_gen_thriller_peliculas.appendChild(image);
+    div_gen_thriller_peliculas.appendChild(titulo);
+
+    //Les doy valor al titulo y a la imagen
+    image.src='https://image.tmdb.org/t/p/w500'+arr_thriller[j].poster_path;
+    titulo.textContent=arr_thriller[j].title;
 }
 
 
