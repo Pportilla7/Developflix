00c25e974d6012b44c3b3b13fa065a59c26aed4d
Pablo Portilla Rivero <pportilla@rfe.es>
2024-02-27 18:35:20 +0100

    Se acab el ejercicio y se consigue el objetivo añadiendo ademas una animacion del tipo hover

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ let div_gen_accion=document.getElementById('genero-28');
 for(let i=0;i<arr_accion.length;i++){
     //Creo un div para cada pelicula y las coloco como hijo de la div genero-28
     let div_gen_accion_peliculas=document.createElement('div');
-    div_gen_accion_peliculas.id='Pelicula_accion'+i;
+    div_gen_accion_peliculas.id='Pelicula_accion_'+i;
     div_gen_accion.appendChild(div_gen_accion_peliculas);
 
     //Creo un titulo y una imagen y los introduzco como hijo de cada div de cada pelicula
@@ -26,6 +26,7 @@ for(let i=0;i<arr_accion.length;i++){
     //Les doy valor al titulo y a la imagen
     image.src='https://image.tmdb.org/t/p/w500'+arr_accion[i].poster_path;
     titulo.textContent=arr_accion[i].title;
+    console.log(div_gen_accion_peliculas);
 }
 
 
@@ -35,7 +36,7 @@ let div_gen_thriller=document.getElementById('genero-53');
 for(let j=0;j<arr_thriller.length;j++){
     //Creo un div para cada pelicula y las coloco como hijo de la div genero-28
     let div_gen_thriller_peliculas=document.createElement('div');
-    div_gen_thriller_peliculas.id='Pelicula_thriller'+j;
+    div_gen_thriller_peliculas.id='Pelicula_thriller_'+j;
     div_gen_thriller.appendChild(div_gen_thriller_peliculas);
 
     //Creo un titulo y una imagen y los introduzco como hijo de cada div de cada pelicula
@@ -49,5 +50,25 @@ for(let j=0;j<arr_thriller.length;j++){
     titulo.textContent=arr_thriller[j].title;
 }
 
+//Para las peliculas de thriller
+let div_gen_aventura=document.getElementById('genero-12');
+
+for(let k=0;k<arr_aventura.length;k++){
+    //Creo un div para cada pelicula y las coloco como hijo de la div genero-28
+    let div_gen_aventura_peliculas=document.createElement('div');
+    console.log(div_gen_aventura_peliculas);
+    div_gen_aventura_peliculas.id='Pelicula_aventura_'+k;
+    div_gen_aventura.appendChild(div_gen_aventura_peliculas);
+
+    //Creo un titulo y una imagen y los introduzco como hijo de cada div de cada pelicula
+    let titulo=document.createElement('h3');
+    let image=document.createElement('img');
+    div_gen_aventura_peliculas.appendChild(image);
+    div_gen_aventura_peliculas.appendChild(titulo);
+
+    //Les doy valor al titulo y a la imagen
+    image.src='https://image.tmdb.org/t/p/w500'+arr_aventura[k].poster_path;
+    titulo.textContent=arr_aventura[k].title;
+}
 
 
